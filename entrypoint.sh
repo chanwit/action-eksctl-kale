dba568e008cfe1c4380a9ebbb716a98e760efd85
Chanwit Kaewkasi <chanwit@gmail.com>
2019-12-10 22:07:14 +0700

    test running pipeline

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,6 @@
+#
+# WORKDIR = /github/workspace
+#
 PATH=/usr/local/bin:$HOME/.local/bin:$PATH
 eksctl utils write-kubeconfig --cluster=${EKSCTL_CLUSTER_NAME}
-kale
+kale --nb ${KALE_NOTEBOOK} --run_pipeline
