3385eca7d6950bddf6d2f9e5df1797c32c87fe9f
Chanwit Kaewkasi <chanwit@gmail.com>
2019-12-10 23:27:17 +0700

    export kubeconfig

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,6 @@
 #
 PATH=/usr/local/bin:$HOME/.local/bin:$PATH
 eksctl utils write-kubeconfig --cluster=${EKSCTL_CLUSTER_NAME}
+
+export KUBECONFIG=/github/home/.kube/config
 kale --nb ${KALE_NOTEBOOK} --run_pipeline
