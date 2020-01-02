7c1affddd32ee66f29fd0bcae36bdf726404bd8d
Chanwit Kaewkasi <chanwit@gmail.com>
2020-01-02 22:59:00 +0700

    add experiment name

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,4 +5,7 @@ PATH=/usr/local/bin:$HOME/.local/bin:$PATH
 eksctl utils write-kubeconfig --cluster=${EKSCTL_CLUSTER_NAME}
 export KUBECONFIG=/github/home/.kube/config
 
-kale --nb ${KALE_NOTEBOOK} --run_pipeline
+EXPERIMENT_NAME="run-${GITHUB_SHA::8}"
+kale --nb ${KALE_NOTEBOOK} --run_pipeline --experiment_name=${EXPERIMENT_NAME}
+
+# wait until the pipeline finish
